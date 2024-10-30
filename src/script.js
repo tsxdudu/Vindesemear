03ba7330d0e0a02a99034122890b8d5b431addbc
tsxdudu <141635170+tsxdudu@users.noreply.github.com>
2024-10-30 18:52:59 -0300

    Create Project finsh

--- src/script.js
+++ src/script.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlayMenu = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+  menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+  menu.classList.remove('abrir-menu')
+})
+
+overlayMenu.addEventListener('click', ()=>{
+  menu.classList.remove('abrir-menu')
+})
