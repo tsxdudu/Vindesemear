d896e095edfad6ad356de7a31aee5bcb76720ad5
tsxdudu <141635170+tsxdudu@users.noreply.github.com>
2024-10-30 19:49:16 -0300

    fix nodejs.

--- src/script.js
+++ src/script.js
@@ -1,29 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".carousel-item");
-    let currentIndex = 0;
-
-    function showNextSlide() {
-      items[currentIndex].classList.remove("active");
-      currentIndex = (currentIndex + 1) % items.length;
-      items[currentIndex].classList.add("active");
-    }
-
-    setInterval(showNextSlide, 3000);
-});
-
-
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlayMenu = document.getElementById('overlay-menu')
 
-btnMenu.addEventListener('click', ()=>{
-  menu.classList.add('abrir-menu')
-})
+btnMenu.addEventListener('click', () => {
+  menu.classList.toggle('abrir-menu');
+});
 
-menu.addEventListener('click', ()=>{
+
+menu.addEventListener('click', () => {
   menu.classList.remove('abrir-menu')
 })
 
-overlayMenu.addEventListener('click', ()=>{
+overlayMenu.addEventListener('click', () => {
   menu.classList.remove('abrir-menu')
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".carousel-item");
+  let currentIndex = 0;
+
+  function showNextSlide() {
+    items[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % items.length;
+    items[currentIndex].classList.add("active");
+  }
+
+  setInterval(showNextSlide, 3000);
+});
+
